0f9a0bf4e9207985a8c6fbe24f18bf55a1abec1d
janrutger <janrutger@jaruku.nl>
2023-01-19 18:48:15 +0100

    thread controll issues

--- assembler/main.asm
+++ assembler/main.asm
@@ -1,5 +1,5 @@
 @main
-speed 0
+speed 500
 iobuff %_plotter
 
 call @oeis
--- christopherSOCK.py
+++ christopherSOCK.py
@@ -5,21 +5,23 @@ import json
 
 from assembler.assembler import Assembler
 from cpu.executer import Executer
-#from machine.keyboard import Keyboard
-#from machine.plotter import Plotter
-from machine.vmachineSock import Machine
+from machine.keyboard import Keyboard
+from machine.plotter import Plotter
+from machine.vmachine import Machine
 from mmu import mmu
 from websock import UIconnect as UI
 
 memory = mmu.MMU()
 ui = UI.UIconnect()
+#VMmachine = threading.Thread(target=runner, args=((memory,)))
+
 
 def runner(memory):
     print("runner")
-    #plotter = Plotter(memory)
-    #keyboard = Keyboard(memory)
+    plotter = Plotter(memory)
+    keyboard = Keyboard(memory)
     executes = Executer(memory, ui)
-    machine = Machine(executes)
+    machine = Machine(executes, plotter, keyboard)
     exitcode = machine.repl()
 
 def start():
@@ -28,10 +30,17 @@ def start():
 def load():
     assembler = Assembler()
 
+    # memory.loadMem(assembler.compile(
+    #     assembler.readASM("./assembler/VMloader.asm")))
+    # memory.loadMem(assembler.compile(
+    #     assembler.readASM("./assembler/VMmain.asm")))
+
     memory.loadMem(assembler.compile(
         assembler.readASM("./assembler/VMloader.asm")))
     memory.loadMem(assembler.compile(
-        assembler.readASM("./assembler/VMmain.asm")))
+        assembler.readASM("./assembler/A133058.asm")))
+    memory.loadMem(assembler.compile(
+        assembler.readASM("./assembler/main.asm")))
 
 
 
@@ -44,7 +53,9 @@ def on_message(wsapp, message):
         load()
     elif input["commando"] == "start":
         print(input)
-        runner(memory)
+        #runner(memory)
+        #VMrunning = True
+        VMmachine.start()
         
 
 def on_open(wsapp):
@@ -55,8 +66,8 @@ def on_open(wsapp):
 
 
 
+VMmachine = threading.Thread(target=runner, args=((memory,)))
 wsapp = websocket.WebSocketApp("ws://localhost:8001/", on_message=on_message,
                                on_open=on_open)
 wsapp.run_forever()
-
 
