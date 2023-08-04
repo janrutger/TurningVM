b0b30e410258e977a23d3c06532d4adc62f1512a
Jan Rutger <janrutger@jaruku.nl>
2023-08-04 21:28:46 +0200

    stuff

--- assembler/out.asm
+++ assembler/out.asm
@@ -1,73 +1,56 @@
 @main
 call @stackssys
-push 1000
-storem $offset
-push 1
-storem $x
-loadm $offset
-call @plot
+push 4
+storem $n
 :_0_condition_start
-loadm $x
-push 180
+loadm $n
+push 1000
 call @neq
 loada
 testz
 clra
 jumpf :_0_repeat_end
-push 180
-loadm $x
-call @minus
-loadm $x
-call @mul
-storem $u
-loadm $offset
-push 4000
-loadm $u
-call @mul
-push 40500
-loadm $u
-call @minus
+loadm $n
+prt
+loadm $n
+push 2
 call @div
-call @plus
-call @plot
-loadm $x
-push 1
-call @plus
-storem $x
-jump :_0_condition_start
-:_0_repeat_end
-push 1
-storem $x
-loadm $offset
-call @plot
 :_1_condition_start
-loadm $x
-push 180
-call @neq
+call @dup
+call @dup
+call @mul
+loadm $n
+call @lt
 loada
 testz
 clra
 jumpf :_1_repeat_end
-push 180
-loadm $x
-call @minus
-loadm $x
-call @mul
-storem $u
-loadm $offset
-push 4000
-loadm $u
-call @mul
-push 40500
-loadm $u
-call @minus
-call @div
-call @minus
-call @plot
-loadm $x
 push 1
-call @plus
-storem $x
+call @minus
 jump :_1_condition_start
 :_1_repeat_end
+storem $i
+loadm $i
+push 1
+call @minus
+call @factorial
+loadm $i
+call @mod
+loadm $i
+push 1
+call @minus
+call @eq
+loada
+testz
+clra
+jumpf :_2_do_end
+loadm $n
+call @plot
+:_2_do_end
+loadm $n
+push 1
+call @plus
+storem $n
+jump :_0_condition_start
+:_0_repeat_end
 ret
