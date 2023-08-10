74a1f19b51848a8ab2a594208094be74abc19f42
Jan Rutger <janrutger@jaruku.nl>
2023-08-10 18:15:12 +0200

    A347754

--- assembler/out.asm
+++ assembler/out.asm
@@ -1,73 +1,59 @@
 @main
 call @stackssys
-push 1000
-storem $offset
 push 1
-storem $x
-loadm $offset
-call @plot
+storem $i
+push 100
+storem $end
+push 1
+storem $s
 :_0_condition_start
-loadm $x
-push 180
+loadm $i
+loadm $end
 call @neq
 loada
 testz
 clra
 jumpf :_0_repeat_end
-push 180
-loadm $x
-call @minus
-loadm $x
+loadm $i
+loadm $i
 call @mul
-storem $u
-loadm $offset
-push 4000
-loadm $u
+loadm $s
+loadm $s
 call @mul
-push 40500
-loadm $u
-call @minus
+call @plus
+storem $j
+loadm $j
+push 2
 call @div
-call @plus
-call @plot
-loadm $x
-push 1
-call @plus
-storem $x
-jump :_0_condition_start
-:_0_repeat_end
-push 1
-storem $x
-loadm $offset
-call @plot
 :_1_condition_start
-loadm $x
-push 180
-call @neq
+call @dup
+call @dup
+call @mul
+loadm $j
+call @lt
 loada
 testz
 clra
 jumpf :_1_repeat_end
-push 180
-loadm $x
-call @minus
-loadm $x
-call @mul
-storem $u
-loadm $offset
-push 4000
-loadm $u
-call @mul
-push 40500
-loadm $u
-call @minus
-call @div
-call @minus
-call @plot
-loadm $x
 push 1
-call @plus
-storem $x
+call @minus
 jump :_1_condition_start
 :_1_repeat_end
+push 1
+call @plus
+storem $k
+loadm $k
+call @plot
+loadm $k
+loadm $k
+call @mul
+loadm $j
+call @minus
+storem $s
+loadm $i
+push 1
+call @plus
+storem $i
+jump :_0_condition_start
+:_0_repeat_end
 ret
