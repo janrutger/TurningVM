c5175cf24bb1835c9d62532061781032c9df1c11
Jan Rutger <janrutger@jaruku.nl>
2023-08-14 22:23:41 +0200

    new divmod function

--- assembler/out.asm
+++ assembler/out.asm
@@ -3,7 +3,7 @@ call @stackssys
 push 0
 storem $quotient
 push 0
-storem $remainder
+storem $counter
 storem $divisor
 storem $number
 loadm $divisor
@@ -16,39 +16,23 @@ jumpf :_0_goto_end
 jump :uitzonderingen
 :_0_goto_end
 :_1_condition_start
-loadm $divisor
-loadm $remainder
+loadm $number
+loadm $counter
 call @lt
 loada
 testz
 clra
 jumpf :_1_repeat_end
-loadm $remainder
+loadm $counter
 loadm $divisor
 call @plus
-storem $remainder
+storem $counter
 loadm $quotient
 push 1
 call @plus
 storem $quotient
 jump :_1_condition_start
 :_1_repeat_end
-loadm $remainder
-loadm $divisor
-call @eq
-loada
-testz
-clra
-jumpf :_2_do_end
-loadm $remainder
-loadm $divisor
-call @plus
-storem $remainder
-loadm $quotient
-push 1
-call @plus
-storem $quotient
-:_2_do_end
 jump :einde
 :uitzonderingen
 push 1
@@ -57,13 +41,21 @@ call @eq
 loada
 testz
 clra
-jumpf :_3_do_end
+jumpf :_2_do_end
 loadm $divisor
 storem $quotient
-push 0
-storem $remainder
-:_3_do_end
+loadm $divisor
+storem $counter
+:_2_do_end
 push 2
+loadm $divisor
+call @eq
+loada
+testz
+clra
+jumpf :_3_do_end
+:_3_do_end
+push 0
 loadm $divisor
 call @eq
 loada
@@ -71,17 +63,9 @@ testz
 clra
 jumpf :_4_do_end
 :_4_do_end
-push 0
-loadm $divisor
-call @eq
-loada
-testz
-clra
-jumpf :_5_do_end
-:_5_do_end
 :einde
 loadm $quotient
+loadm $counter
 loadm $number
-loadm $remainder
 call @minus
 ret
