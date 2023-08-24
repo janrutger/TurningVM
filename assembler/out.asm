c8958b562523e37cc9cbb3cf4a78d6ca7e128004
Jan Rutger <janrutger@jaruku.nl>
2023-08-24 21:53:22 +0200

    STACKS added TIMER instrcutions

--- assembler/out.asm
+++ assembler/out.asm
@@ -1,16 +1,47 @@
 @main
 settimer 0
 call @stackssys
-push 23
-storem $time
+push 4
+storem $number
+call @input
+storem $end
+:_0_condition_start
+loadm $number
+loadm $end
+call @neq
+loada
+testz
+clra
+jumpf :_0_repeat_end
 settimer 16
-gettimer 16
-loadm $time
-call @sleep
-gettimer 16
-prttimer 16
-call @swap
+loadm $number
+loadm $number
+push 2
+call @div
+:_1_condition_start
+call @dup
+call @dup
+call @mul
+loadm $number
+call @lt
+loada
+testz
+clra
+jumpf :_1_repeat_end
+push 1
 call @minus
-prt
+jump :_1_condition_start
+:_1_repeat_end
+call @dup
+call @mul
+call @minus
+call @plot
+loadm $number
+push 1
+call @plus
+storem $number
+prttimer 16
+jump :_0_condition_start
+:_0_repeat_end
 prttimer 0
 ret
