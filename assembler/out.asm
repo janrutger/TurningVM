49c02957a118750943d561a3d168d8c90209f6b7
Jan Rutger <janrutger@jaruku.nl>
2023-08-26 13:37:09 +0200

    added A000120

--- assembler/out.asm
+++ assembler/out.asm
@@ -1,27 +1,25 @@
 @main
 settimer 0
 call @stackssys
-speed 5
-push 1
-storem $n
+push 0
 call @input
 storem $einde
 :_0_condition_start
-loadm $n
+push 1
+call @plus
+call @dup
+call @dup
 loadm $einde
 call @neq
 loada
 testz
 clra
 jumpf :_0_repeat_end
-loadm $n
 call @abs
 call @plot
-loadm $n
-push 1
-call @plus
-storem $n
 jump :_0_condition_start
 :_0_repeat_end
+pull
+pull
 prttimer 0
 ret
