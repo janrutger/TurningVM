9a9dd1f0b7d8e83248f71c81aafdd36a6dd8a377
Jan Rutger <janrutger@jaruku.nl>
2024-02-07 19:28:07 +0100

    ISQRT binary search

--- assembler/out.asm
+++ assembler/out.asm
@@ -1,23 +1,55 @@
 @main
 settimer 0
 speed 0
-push 1
-storem $y
-:_0_condition_start
-loadm $y
-push 2000
+push 4
+storem $n
+:_2_condition_start
+loadm $n
+push 1000
 call @neq
 loada
 testz
 clra
-jumpf :_0_repeat_end
+jumpf :_2_repeat_end
+settimer 16
+loadm $n
+call @~isqrt
+storem $i
+loadm $i
+push 1
+call @minus
+call @factorial
+loadm $i
+call @mod
+loadm $i
+push 1
+call @minus
+call @eq
+loada
+testz
+clra
+jumpf :_3_do_end
+loadm $n
+call @plot
+:_3_do_end
+prttimer 16
+loadm $n
+push 1
+call @plus
+storem $n
+jump :_2_condition_start
+:_2_repeat_end
+prttimer 0
+ret
+@~isqrt
+storem $y
 push 0
 storem $L
 loadm $y
 push 1
 call @plus
 storem $R
-:_1_condition_start
+:_0_condition_start
 loadm $L
 loadm $R
 push 1
@@ -26,7 +58,7 @@ call @neq
 loada
 testz
 clra
-jumpf :_1_repeat_end
+jumpf :_0_repeat_end
 loadm $L
 loadm $R
 call @plus
@@ -41,23 +73,15 @@ call @lt
 loada
 testz
 clra
-jumpf :_2_do_end
+jumpf :_1_do_end
 loadm $M
 storem $R
 jump :done
-:_2_do_end
+:_1_do_end
 loadm $M
 storem $L
 :done
-jump :_1_condition_start
-:_1_repeat_end
-loadm $L
-call @plot
-loadm $y
-push 1
-call @plus
-storem $y
 jump :_0_condition_start
 :_0_repeat_end
-prttimer 0
+loadm $L
 ret
