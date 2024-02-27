f422ad2c69cfe6e3870b466017f36c4919826d5c
Jan Rutger <janrutger@jaruku.nl>
2024-02-27 16:07:54 +0100

    Stacks V3.1 arrays in STACKS

--- assembler/out.asm
+++ assembler/out.asm
@@ -12,6 +12,7 @@ push 'jobinput_test'
 index *A1
 push 'jobinput_test'
 job @~test
+join
 :getResult
 result
 jumpf :_1_no_result
@@ -33,6 +34,44 @@ jump :klaar
 :_1_no_result
 jump :getResult
 :klaar
+push 0
+storem $n
+array *A3
+push 4
+storem *A3
+push 8
+storem *A3
+push 16
+storem *A3
+array *A4
+push 3
+storem *A4
+push 6
+storem *A4
+push 9
+storem *A4
+:_3_condition_start
+loadm *A3
+call @~step
+call @lt
+loada
+testz
+clra
+jumpf :_3_repeat_end
+loadm $n
+readelm *A3
+jumpt :_4_readelm_done
+call @__illegal_Array_Index
+:_4_readelm_done
+loadm $n
+readelm *A4
+jumpt :_5_readelm_done
+call @__illegal_Array_Index
+:_5_readelm_done
+call @mul
+prt
+jump :_3_condition_start
+:_3_repeat_end
 prttimer 0
 ret
 @~test
@@ -54,3 +93,10 @@ storem $_0_A1
 jump :_0_start_each
 :_0_end_each
 done *A2
+@~step
+loadm $n
+loadm $n
+push 1
+call @plus
+storem $n
+ret
