04ace4835a6387fae73bffd6beb62181712a30da
Jan Rutger <janrutger@jaruku.nl>
2024-05-19 18:46:46 +0200

    More linedraw

--- assembler/out.asm
+++ assembler/out.asm
@@ -2,21 +2,21 @@
 settimer 0
 speed 0
 array *P3
-push 30
+push 61
 storem *P3
 push 50
 storem *P3
 array *P2
-push 10
+push 81
 storem *P2
-push 10
+push 20
 storem *P2
 array *P1
-push 50
+push 40
 storem *P1
-push 10
+push 20
 storem *P1
-push 0
+push 1
 call @drawRate
 call @drawNew
 push 0
@@ -36,61 +36,72 @@ call @draw
 push 0
 push 0
 call @~(Turtle)_goto
-loadm $x
-readelm *P2
-jumpt :_55_readelm_done
-call @__illegal_Array_Index
-:_55_readelm_done
-loadm $y
-readelm *P2
-jumpt :_56_readelm_done
-call @__illegal_Array_Index
-:_56_readelm_done
-call @~(Turtle)_goto
-loadm $x
-readelm *P1
-jumpt :_57_readelm_done
-call @__illegal_Array_Index
-:_57_readelm_done
-loadm $y
-readelm *P1
-jumpt :_58_readelm_done
-call @__illegal_Array_Index
-:_58_readelm_done
-call @~(Turtle)_goto
 call @drawBuff
-loadm $x
-readelm *P2
-jumpt :_59_readelm_done
-call @__illegal_Array_Index
-:_59_readelm_done
-loadm $y
-readelm *P2
-jumpt :_60_readelm_done
-call @__illegal_Array_Index
-:_60_readelm_done
+push 10
+push 10
+call @~(Turtle)_goto
+push 140
+push 15
 call @~(Turtle)_line
-loadm $x
-readelm *P3
-jumpt :_61_readelm_done
-call @__illegal_Array_Index
-:_61_readelm_done
-loadm $y
-readelm *P3
-jumpt :_62_readelm_done
-call @__illegal_Array_Index
-:_62_readelm_done
+push 10
+push 10
+call @~(Turtle)_goto
+push 140
+push 20
 call @~(Turtle)_line
-loadm $x
-readelm *P1
-jumpt :_63_readelm_done
-call @__illegal_Array_Index
-:_63_readelm_done
-loadm $y
-readelm *P1
-jumpt :_64_readelm_done
-call @__illegal_Array_Index
-:_64_readelm_done
+push 10
+push 10
+call @~(Turtle)_goto
+push 540
+push 30
+call @~(Turtle)_line
+push 10
+push 10
+call @~(Turtle)_goto
+push 540
+push 50
+call @~(Turtle)_line
+push 10
+push 10
+call @~(Turtle)_goto
+push 540
+push 170
+call @~(Turtle)_line
+push 10
+push 10
+call @~(Turtle)_goto
+push 70
+push 270
+call @~(Turtle)_line
+push 10
+push 10
+call @~(Turtle)_goto
+push 50
+push 270
+call @~(Turtle)_line
+push 10
+push 10
+call @~(Turtle)_goto
+push 40
+push 70
+call @~(Turtle)_line
+push 10
+push 10
+call @~(Turtle)_goto
+push 35
+push 270
+call @~(Turtle)_line
+push 10
+push 10
+call @~(Turtle)_goto
+push 20
+push 70
+call @~(Turtle)_line
+push 10
+push 10
+call @~(Turtle)_goto
+push 15
+push 70
 call @~(Turtle)_line
 call @drawBuff
 prttimer 0
@@ -123,6 +134,8 @@ call @dup
 prt
 ret
 @~(Turtle)_move
+loadm $(Turtle)_angle
+prt
 loadm $(Turtle)_angle
 push 22
 call @plus
@@ -419,7 +432,7 @@ call @char2prtbuff
 call @printbuff
 call @~(Turtle)_slope
 call @dup
-push 100
+loadm $(Turtle)_high
 call @lt
 loada
 testz
@@ -432,7 +445,7 @@ call @~(Turtle)_move
 jump :_31_match_end
 :_32_do_end
 call @dup
-push 5
+loadm $(Turtle)_low
 call @gt
 loada
 testz
@@ -464,7 +477,7 @@ call @char2prtbuff
 call @printbuff
 call @~(Turtle)_slope
 call @dup
-push 100
+loadm $(Turtle)_high
 call @lt
 loada
 testz
@@ -477,7 +490,7 @@ call @~(Turtle)_move
 jump :_35_match_end
 :_36_do_end
 call @dup
-push 5
+loadm $(Turtle)_low
 call @gt
 loada
 testz
@@ -525,7 +538,7 @@ call @char2prtbuff
 call @printbuff
 call @~(Turtle)_slope
 call @dup
-push 100
+loadm $(Turtle)_high
 call @lt
 loada
 testz
@@ -538,7 +551,7 @@ call @~(Turtle)_move
 jump :_41_match_end
 :_42_do_end
 call @dup
-push 5
+loadm $(Turtle)_low
 call @gt
 loada
 testz
@@ -570,7 +583,7 @@ call @char2prtbuff
 call @printbuff
 call @~(Turtle)_slope
 call @dup
-push 100
+loadm $(Turtle)_high
 call @lt
 loada
 testz
@@ -583,7 +596,7 @@ call @~(Turtle)_move
 jump :_45_match_end
 :_46_do_end
 call @dup
-push 7
+loadm $(Turtle)_low
 call @gt
 loada
 testz
@@ -689,9 +702,9 @@ ret
 @__MemAllocGlobels
 call @init_vmachine
 array *size
-push 160
+push 640
 storem *size
-push 80
+push 320
 storem *size
 push 0
 storem $x
@@ -707,6 +720,10 @@ push 0
 storem $(Turtle)_x
 push 0
 storem $(Turtle)_y
+push 1
+storem $(Turtle)_low
+push 90
+storem $(Turtle)_high
 push 0
 storem $(Turtle)_targetX
 push 0
